7abf920dc5d36d23bf78d0bd0cb8288d5b03c70d
Gideon Pfeffer <gap16@duke.edu>
2017-09-02 18:26:43 -0400

    added searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar'
 
 class App extends Component {
   render() {
     return (
-      <div>Testing</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
